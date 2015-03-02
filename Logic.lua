33a922cc744714824ff7cf9f4371f9245cff6b6d
Bearz <TechnicalBear@gmail.com>
2015-03-02 18:20:45 +0000

    Logic experiments
    If "half life 3" is fed into the console it will print "is amazing <3"
    if not it will just repeat what you put in, it also has a timer that
    keeps the window open after the command is given to keep da console up

---
+++ Logic.lua
@@ -0,0 +1,15 @@
+local input = nill
+
+print("Enter Somthing!!")
+input = io.read()
+
+if input == "half life 3" then
+	 print ("Is amazing <3")
+	 local timer = os.time()
+	 repeat until os.time() > timer + 1 
+else
+	print ("You Entered: "..imput)
+	local timer = os.time()
+	repeat until os.time() > timer + 1
+end 
+
